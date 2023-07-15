88e9c2dc240271ad9bc7b822849072b5a3fe9b94
KetanKochhar <139612962+KetanKochhar@users.noreply.github.com>
2023-07-15 13:04:47 +0530

    Add files via upload

---
+++ Kids alphabets learner/source code/main.js
@@ -0,0 +1,351 @@
+// alert("hello")
+
+// let a = new Audio('audio/a.mp3')
+// a.play()
+
+let d1 = document.getElementById("1")
+let d2 = document.getElementById("2")
+let d3 = document.getElementById("3")
+let d4 = document.getElementById("4")
+let d5 = document.getElementById("5")
+let d6 = document.getElementById("6")
+let d7 = document.getElementById("7")
+let d8 = document.getElementById("8")
+let d9 = document.getElementById("9")
+let d10 = document.getElementById("10")
+let d11 = document.getElementById("11")
+let d12 = document.getElementById("12")
+let d13 = document.getElementById("13")
+let d14 = document.getElementById("14")
+let d15 = document.getElementById("15")
+let d16 = document.getElementById("16")
+let d17 = document.getElementById("17")
+let d18 = document.getElementById("18")
+let d19 = document.getElementById("19")
+let d20 = document.getElementById("20")
+let d21 = document.getElementById("21")
+let d22 = document.getElementById("22")
+let d23 = document.getElementById("23")
+let d24 = document.getElementById("24")
+let d25 = document.getElementById("25")
+let d26 = document.getElementById("26")
+let all = document.getElementsByClassName("pop")
+
+let a = document.getElementById("a")
+let b = document.getElementById("b")
+let c = document.getElementById("c")
+let d = document.getElementById("d")
+let e = document.getElementById("e")
+let f = document.getElementById("f")
+let g = document.getElementById("g")
+let h = document.getElementById("h")
+let i = document.getElementById("i")
+let j = document.getElementById("j")
+let k = document.getElementById("k")
+let l = document.getElementById("l")
+let m = document.getElementById("m")
+let n = document.getElementById("n")
+let o = document.getElementById("o")
+let p = document.getElementById("p")
+let q = document.getElementById("q")
+let r = document.getElementById("r")
+let s = document.getElementById("s")
+let t = document.getElementById("t")
+let u = document.getElementById("u")
+let v = document.getElementById("v")
+let w = document.getElementById("w")
+let x = document.getElementById("x")
+let y = document.getElementById("y")
+let z = document.getElementById("z")
+
+
+function s1() {
+  d1.style.display = "block"
+  a.play()
+}
+function h1() {
+  d1.style.display = "none"
+}
+function s2() {
+  d2.style.display = "block"
+  b.play()
+}
+function h2() {
+  d2.style.display = "none"
+}
+function s3() {
+  d3.style.display = "block"
+  c.play()
+}
+function h3() {
+  d3.style.display = "none"
+}
+function s4() {
+  d4.style.display = "block"
+  d.play()
+}
+function h4() {
+  d4.style.display = "none"
+}
+function s5() {
+  d5.style.display = "block"
+  e.play()
+}
+function h5() {
+  d5.style.display = "none"
+}
+function s6() {
+  d6.style.display = "block"
+  f.play()
+}
+function h6() {
+  d6.style.display = "none"
+}
+function s7() {
+  d7.style.display = "block"
+  g.play()
+}
+function h7() {
+  d7.style.display = "none"
+}
+function s8() {
+  d8.style.display = "block"
+  h.play()
+}
+function h8() {
+  d8.style.display = "none"
+}
+function s9() {
+  d9.style.display = "block"
+  i.play()
+}
+function h9() {
+  d9.style.display = "none"
+}
+function s10() {
+  d10.style.display = "block"
+  j.play()
+}
+function h10() {
+  d10.style.display = "none"
+}
+function s11() {
+  d11.style.display = "block"
+  k.play()
+}
+function h11() {
+  d11.style.display = "none"
+}
+function s12() {
+  d12.style.display = "block"
+  l.play()
+}
+function h12() {
+  d12.style.display = "none"
+}
+function s13() {
+  d13.style.display = "block"
+  m.play()
+}
+function h13() {
+  d13.style.display = "none"
+}
+function s14() {
+  d14.style.display = "block"
+  n.play()
+}
+function h14() {
+  d14.style.display = "none"
+}
+function s15() {
+  d15.style.display = "block"
+  o.play()
+}
+function h15() {
+  d15.style.display = "none"
+}
+function s16() {
+  d16.style.display = "block"
+  p.play()
+}
+function h16() {
+  d16.style.display = "none"
+}
+function s17() {
+  d17.style.display = "block"
+  q.play()
+}
+function h17() {
+  d17.style.display = "none"
+}
+function s18() {
+  d18.style.display = "block"
+  r.play()
+}
+function h18() {
+  d18.style.display = "none"
+}
+function s19() {
+  d19.style.display = "block"
+  s.play()
+}
+function h19() {
+  d19.style.display = "none"
+}
+function s20() {
+  d20.style.display = "block"
+  t.play()
+}
+function h20() {
+  d20.style.display = "none"
+}
+function s21() {
+  d21.style.display = "block"
+  u.play()
+}
+function h21() {
+  d21.style.display = "none"
+}
+function s22() {
+  d22.style.display = "block"
+  v.play()
+}
+function h22() {
+  d22.style.display = "none"
+}
+function s23() {
+  d23.style.display = "block"
+  w.play()
+}
+function h23() {
+  d23.style.display = "none"
+}
+function s24() {
+  d24.style.display = "block"
+  x.play()
+}
+function h24() {
+  d24.style.display = "none"
+}
+function s25() {
+  d25.style.display = "block"
+  y.play()
+}
+function h25() {
+  d25.style.display = "none"
+}
+function s26() {
+  d26.style.display = "block"
+}
+function h26() {
+  d26.style.display = "none"
+  z.play()
+}
+
+document.addEventListener("keypress", function (event) {
+  if (event.keyCode == 97) {
+    // alert('hi.');
+    s1()
+    setTimeout("h1()", 3000)
+  }
+  else if (event.keyCode == 98) {
+    s2()
+    setTimeout("h2()", 3000)
+  }
+  else if (event.keyCode == 99) {
+    s3()
+    setTimeout("h3()", 3000)
+  }
+  else if (event.keyCode == 100) {
+    s4()
+    setTimeout("h4()", 3000)
+  }
+  else if (event.keyCode == 101) {
+    s5()
+    setTimeout("h5()", 3000)
+  }
+  else if (event.keyCode == 102) {
+    s6()
+    setTimeout("h6()", 3000)
+  }
+  else if (event.keyCode == 103) {
+    s7()
+    setTimeout("h7()", 3000)
+  }
+  else if (event.keyCode == 104) {
+    s8()
+    setTimeout("h8()", 3000)
+  }
+  else if (event.keyCode == 105) {
+    s9()
+    setTimeout("h9()", 3000)
+  }
+  else if (event.keyCode == 106) {
+    s10()
+    setTimeout("h10()", 3000)
+  }
+  else if (event.keyCode == 107) {
+    s11()
+    setTimeout("h11()", 3000)
+  }
+  else if (event.keyCode == 108) {
+    s12()
+    setTimeout("h12()", 3000)
+  }
+  else if (event.keyCode == 109) {
+    s13()
+    setTimeout("h13()", 3000)
+  }
+  else if (event.keyCode == 110) {
+    s14()
+    setTimeout("h14()", 3000)
+  }
+  else if (event.keyCode == 111) {
+    s15()
+    setTimeout("h15()", 3000)
+  }
+  else if (event.keyCode == 112) {
+    s16()
+    setTimeout("h16()", 3000)
+  }
+  else if (event.keyCode == 113) {
+    s17()
+    setTimeout("h17()", 3000)
+  }
+  else if (event.keyCode == 114) {
+    s18()
+    setTimeout("h18()", 3000)
+  }
+  else if (event.keyCode == 115) {
+    s19()
+    setTimeout("h19()", 3000)
+  }
+  else if (event.keyCode == 116) {
+    s20()
+    setTimeout("h20()", 3000)
+  }
+  else if (event.keyCode == 117) {
+    s21()
+    setTimeout("h21()", 3000)
+  }
+  else if (event.keyCode == 118) {
+    s22()
+    setTimeout("h22()", 3000)
+  }
+  else if (event.keyCode == 119) {
+    s23()
+    setTimeout("h23()", 3000)
+  }
+  else if (event.keyCode == 120) {
+    s24()
+    setTimeout("h24()", 3000)
+  }
+  else if (event.keyCode == 121) {
+    s25()
+    setTimeout("h25()", 3000)
+  }
+  else if (event.keyCode == 122) {
+    s26()
+    setTimeout("h26()", 3000)
+  }
+});
